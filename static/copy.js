4bb52bad617a444b5c2cc8fe698dde47a9c735fb
RuiMk2 <109416949+RuiMk2@users.noreply.github.com>
2024-04-30 17:23:54 +0800

    added draw winner in own website

--- static/copy.js
+++ static/copy.js
@@ -6,3 +6,19 @@ function Copy() {
     // Put the text to the clipboard
     navigator.clipboard.writeText(copyText.value);
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function drawWinner() {
+    entries = document.getElementById("generate").innerHTML.split(/\r?\n/)
+    // remove last element as it is always blank due to using new lines for the for loop
+    entries.pop()
+    for (let count = 0; count < 10; count++) {
+        winner = Math.floor(Math.random() * entries.length)
+        document.getElementById("winner").innerHTML = entries[winner]
+        await sleep(count * 100)
+    }
+    document.getElementById("winner").innerHTML = "Winner: " + entries[winner]
+}
